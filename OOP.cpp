35251eac4e20ff6dd08ef542649262cb58253da3
julianferdiyanto <julianferdiyanto3@gmail.com>
2025-04-29 10:48:35 +0700

    menambahkan member class

--- OOP.cpp
+++ OOP.cpp
@@ -3,4 +3,12 @@ using namespace std;
 
 class mahasiswa{
     public:
+    int nim;
+    string nama;
+    float nilai;
+    void print_data(){
+        cout << "NIM = " << nim << endl;
+        cout << "Nama = " << nama << endl;
+        cout << "Nilai = " << nilai << endl;
+    }
 };
